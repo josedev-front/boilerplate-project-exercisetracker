bb6e56ab7d92f8b34d8ffa67cb202cf9b99b43f6
josedev-front <130413063+josedev-front@users.noreply.github.com>
2024-04-23 23:14:02 -0400

    ss
    ss

--- index.js
+++ index.js
@@ -2,100 +2,164 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const users = [];
-
-function User(username, id) {
-    this.username = username;
-    this._id = id;
-    this.log = [];
-}
-
-users.push(new User('testUser', '0'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 
 app.use(cors())
 app.use(express.static('public'))
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html')
 });
 
-app.route('/api/users')
-    .get((req, res) => {
-        res.json(users)
-    })
-    .post((req, res) => {
-        const id = users.length
-        const username = req.body.username
-        users.push(new User(username, id))
-        res.json({username: username, _id: id})
-    })
+const mySecret = process.env['MONGO_URI']
 
-app.post('/api/users/:_id/exercises', (req, res) => {
-    const description = req.body.description;
-    const duration = +req.body.duration;
-    const date = req.body.date;
-    const id = req.params._id;
-    const user = users[id]
-    if (!duration || !description){
-        return res.json({error: 'missing parameters'})
-    } else {
-        let dateString;
-        if (date) {
-            dateString = new Date(date).toDateString()
-        } else {
-            dateString = new Date().toDateString()
-        }
+main().catch(err => console.log(err));
 
-        user.log.push({description: description, duration: duration, date: dateString});
-        user.count = user.log.length;
-        res.json({
-            username: user.username,
-            description: description,
-            duration: duration,
-            date: dateString,
-            _id: user._id
-        })
+async function main() {
+  await mongoose.connect(mySecret);
+}
+//-----------
+const userSchema = mongoose.Schema({
+  username: String,
+  _id: String
+});
+const exerciseSchema = mongoose.Schema({
+  duser: String,
+  description:{
+    type: String,
+    required: true
+  },
+  duration:{
+      type: Number,
+      required: true
+  },
+  date: Date
+});
+
+var User = mongoose.model('user',userSchema);
+var exercise = mongoose.model('exercise',exerciseSchema);
+//------------
+app.post('/api/users',async function(req,res){
+  const user = req.body.username;
+  try{
+    const username = await User.findOne({username: user});
+    if(username == null){
+      let newId = new mongoose.mongo.ObjectId();
+      const newUser = new User({username: req.body.username, _id: newId});
+      await newUser.save();
+      res.json({'username':newUser.username,'_id': newUser._id});
+    }else{
+      res.json({'username':username.username,'_id': username._id});
     }
+
+  }catch(err){
+
+    res.status(500).json(err);
+  }
+});
+//--------------------
+app.get('/api/users',async function(req,res){
+  try{
+    var users = [];
+    users = await User.find();
+    res.json(users);
+  }catch(err){
+    res.status(500).json(err);
+  }
+
 })
 
-app.get("/api/users/:_id/logs", async (req, res) => {
-  const userId = req.params._id;
-  const user = await User.findById(userId).select("username log");
+app.post('/api/users/:_id/exercises', async function(req,res){
+  const id = req.params._id;
+  const data = req.body;
 
-  if (!user) {
-    return res.status(404).json({ error: "User not found" });
+  let date;
+  if(data.date == null || data.date == ''){
+    date = new Date();
+  }else{
+    date = data.date;
   }
 
-  let { from, to, limit } = req.query;
+  try{
+    const newexer = new exercise({
+      duser: id,
+      description:data.description,
+      duration: data.duration,
+      date:date
+    });
+    await newexer.save();
+    const user = await User.findById(id).exec();
 
-  let fromDate = from ? new Date(from) : new Date(0);
-  let toDate = to ? new Date(to) : new Date();
-  limit = limit ? parseInt(limit) : 0;
 
-  user.log = user.log.filter((entry) => {
-    const entryDate = new Date(entry.date);
-    return entryDate >= fromDate && entryDate <= toDate;
-  });
+    res.json({
+      username:  user.username,
+      _id: user._id,
+      description:newexer.description,
+      duration:newexer.duration,
+      date: new Date(newexer.date).toDateString()
 
-  if (limit > 0) {
-    user.log = user.log.slice(0, limit);
+    });
+  }catch(err){
+    res.status(500).json(err);
+
   }
-
-  // Formatear la propiedad de fecha en cada registro como una cadena en formato de fecha de la API de JavaScript
-  user.log.forEach((entry) => {
-    entry.date = new Date(entry.date).toDateString();
-  });
-
-  res.json({
-    _id: user._id,
-    username: user.username,
-    count: user.log.length,
-    log: user.log,
-  });
 });
 
+app.get('/api/users/:_id/exercises', async function(req,res){
+  const id = req.params._id;
+  try{
+    const exers = await exercise.find({duser: id});
+    res.json(exers);
+ }catch(err){
+    res.status(500).json(err);
+
+  }
+})
+
+app.get('/api/users/:_id/logs',async function(req,res){
+  const id = req.params._id;
+  const { from, to, limit } = req.query;
+  let dateObj = {}
+  if (from) {
+    dateObj["$gte"] = new Date(from)
+  }
+  if (to){
+    dateObj["$lte"] = new Date(to)
+  }
+  let filter = {
+    duser: id
+  }
+  if(from || to){
+    filter.date = dateObj;
+  }
+
+  try{
+    const user = await User.findById(id).exec();
+    const exers = await exercise.find(filter).limit(+limit ?? 500);
+    const num = await exercise.find({ duser: id}).count().exec();
+    let log = exers.map((e) => {
+            return {
+                'description': e.
+description
+,
+              'duration': e.duration,
+                'date': new Date(e.date).toDateString()
+        };
+    });
+    res.json({
+      username: user.username,
+      count : num,
+      _id: user._id,
+     log});
+ }catch(err){
+    res.status(500).json(err);
+
+  }
+})
+
 const listener = app.listen(process.env.PORT || 3000, () => {
-    console.log('Your app is listening on port ' + listener.address().port)
+  console.log('Your app is listening on port ' + listener.address().port)
 })
