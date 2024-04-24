2ca13cc5fcd1ac54b4269eb954cca18a9126a74c
josedev-front <130413063+josedev-front@users.noreply.github.com>
2024-04-23 23:08:44 -0400

    f
    f

--- index.js
+++ index.js
@@ -60,23 +60,41 @@ app.post('/api/users/:_id/exercises', (req, res) => {
     }
 })
 
-app.route('/api/users/:_id/logs')
-    .get((req, res) => {
-        const id = req.params._id;
-        const limit = Number(req.query.limit) || 0;
-        const response = users[id];
-        let newlog = response.log
-        if (req.query.from && req.query.to) {
-            const from = new Date(req.query.from).getTime();
-            const to = new Date(req.query.to).getTime();
-            newlog = newlog.filter(item => from <= new Date(item.date).getTime() <= to)}
+app.get("/api/users/:_id/logs", async (req, res) => {
+  const userId = req.params._id;
+  const user = await User.findById(userId).select("username log");
 
-        if (limit > 0){
-            newlog = newlog.slice(0, limit)
-        }
-        response.log = newlog
-        res.json(response)
-    })
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  let { from, to, limit } = req.query;
+
+  let fromDate = from ? new Date(from) : new Date(0);
+  let toDate = to ? new Date(to) : new Date();
+  limit = limit ? parseInt(limit) : 0;
+
+  user.log = user.log.filter((entry) => {
+    const entryDate = new Date(entry.date);
+    return entryDate >= fromDate && entryDate <= toDate;
+  });
+
+  if (limit > 0) {
+    user.log = user.log.slice(0, limit);
+  }
+
+  // Formatear la propiedad de fecha en cada registro como una cadena en formato de fecha de la API de JavaScript
+  user.log.forEach((entry) => {
+    entry.date = new Date(entry.date).toDateString();
+  });
+
+  res.json({
+    _id: user._id,
+    username: user.username,
+    count: user.log.length,
+    log: user.log,
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('Your app is listening on port ' + listener.address().port)
